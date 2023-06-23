7dd5609988777a22697b8024806dfb511ced880b
AyanaZaire <ayanazaire@gmail.com>
2023-06-23 19:04:42 -0400

    updated port

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.use(cors())
 app.use(express.static('public'))
 
 // Serve Static File 
-app.get('https://anthology-altar-17f92dfe2b72.herokuapp.com/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port 3000')
 })
 
