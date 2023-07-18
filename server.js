f247dd45267b0c220254ad11af47cce59fc26457
AyanaZaire <ayanazaire@gmail.com>
2023-07-18 11:26:15 -0400

    updated config var

--- server.js
+++ server.js
@@ -31,10 +31,9 @@ app.get('/', (req, res) => {
 });
 
 // for heroku deployment
-const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-app.listen(port, host, () => {
+app.listen(port, () => {
   console.log('Your app is listening on port 3000')
 })
 
